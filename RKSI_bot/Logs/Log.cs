c992b7607dda6e9335787a97ed046453b46295e8
Anton Antonov <gzalypov@bk.ru>
2021-09-18 22:39:49 +0300

    Add skiped classes

---
+++ RKSI_bot/Logs/Log.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Telegram.Bot.Args;
+
+namespace RKSI_bot.Logs
+{
+    abstract class Log
+    {
+        public void SetLog(MessageEventArgs messageInfo)
+        {
+            var message = messageInfo.Message;
+
+            string textLog = message.Text + $"|| {message.Chat.Id} - {message.Chat.FirstName} : {message.Chat.LastName} -- {DateTime.Now} | Username - {message.Chat.Username}";
+
+            PrintLog(textLog);
+        }
+
+        public void SetLog(CallbackQueryEventArgs messageInfo)
+        {
+            var message = messageInfo.CallbackQuery.Message;
+
+            string textLog = message.Text + $"|{message.Chat.Username} | {message.Chat.Id} - {message.Chat.FirstName} : {message.Chat.LastName} -- {DateTime.Now} | Username - {message.Chat.Username}";
+
+            PrintLog(textLog);
+        }
+
+        public abstract void PrintLog(string message);
+    }
+}
---
+++ RKSI_bot/Logs/LogConsole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RKSI_bot.Logs
+{
+    class LogConsole : Log
+    {
+        public override void PrintLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
