30575b88da076477ff82aa2f47ba2460a9de3518
Grigoriy Paravyan <gvalal@bk.ru>
2021-12-14 10:57:59 +0300

    Добавляю StartupConfig

---
+++ RKSI_bot/StartupConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Text;
+
+namespace RKSI_bot
+{
+    public class StartupConfig
+    {
+        private static StartupConfig startup = new StartupConfig();
+        public IConfiguration Configuration { get; }
+
+        public StartupConfig()
+        {
+            Configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+        }
+
+        public static StartupConfig GetInstance()
+        {
+            return startup;
+        }
+    }
+}
---
+++ RksiBot_Tests/Config/StartupConfig_Test.cs
@@ -0,0 +1,21 @@
+﻿using RKSI_bot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RksiBot_Tests.Config
+{
+    public class StartupConfig_Test
+    {
+        [Fact]
+        public void GetInstance_GetConnectionStrings_ReturnNotNullValue()
+        {
+            var config = StartupConfig.GetInstance().Configuration;
+
+            string jsonValue = config.GetSection("ConnectionStrings")["FirstStringConnection"];
+
+            Assert.NotNull(jsonValue);
+        }
+    }
+}
